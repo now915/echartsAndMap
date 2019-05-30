c97eca27a79f7b619f2a41f4d11abc061b2eeeb1
now915 <742766223@qq.com>
2019-05-30 21:55:58 +0800

    create client project

---
+++ client/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import '@/assets/border.css'
+import '@/assets/reset.css'
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  font-size: 14px;
+}
+</style>
